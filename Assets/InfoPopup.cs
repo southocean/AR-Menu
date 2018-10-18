daadc2de907a999518fc7dffc3b34f98ab2d0905
Nam Nguyen Hai <hainam2511@gmail.com>
2018-10-18 17:35:46 +0200

    Auto stash before rebase of "AR-Menu/master"

---
+++ Assets/InfoPopup.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class InfoPopup : MonoBehaviour {
+
+    string btnName;
+    public GameObject panel1, panel2, panel3, panel4;
+    public GameObject menutxt1, menutxt2, menutxt3;
+
+    void setPopupActive(int i)
+    {
+        switch (i)
+        {
+            case 0:
+                panel1.SetActive(false);
+                panel2.SetActive(false);
+                panel3.SetActive(false);
+                panel4.SetActive(false);
+                break;
+            case 1:
+                panel1.SetActive(true);
+                panel2.SetActive(false);
+                panel3.SetActive(false);
+                panel4.SetActive(false);
+                break;
+            case 2:
+                panel1.SetActive(false);
+                panel2.SetActive(true);
+                panel3.SetActive(false);
+                panel4.SetActive(false);
+                break;
+            case 3:
+                panel1.SetActive(false);
+                panel2.SetActive(false);
+                panel3.SetActive(true);
+                panel4.SetActive(false);
+                break;
+            case 4:
+                panel1.SetActive(false);
+                panel2.SetActive(false);
+                panel3.SetActive(false);
+                panel4.SetActive(true);
+                break;
+            default:
+                break;
+        }
+    }
+
+    void selectMenu(int i)
+    {
+        switch (i)
+        {
+            case 1:
+                SceneManager.LoadScene("main");
+                break;
+            case 2:
+                SceneManager.LoadScene("sidedishes");
+                break;
+            case 3:
+                SceneManager.LoadScene("drinks");
+                break;
+            default:
+                break;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                btnName = hit.transform.name;
+                switch (btnName)
+                {
+                    case "Food1":
+                        setPopupActive(1);
+                        break;
+                    case "Food2":
+                        setPopupActive(2);
+                        break;
+                    case "Food3":
+                        setPopupActive(3);
+                        break;
+                    case "Food4":
+                        setPopupActive(4);
+                        break;
+                    case "Menu1":
+                        selectMenu(1);
+                        break;
+                    case "Menu2":
+                        selectMenu(2);
+                        break;
+                    case "Menu3":
+                        selectMenu(3);
+                        break;
+                    default:
+                        setPopupActive(0);
+                        break;
+                }
+            }
+            else
+            {
+                setPopupActive(0);
+            }
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Debug.Log("Left mouse clicked!");
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                //Debug.Log(hit.transform.name);
+                btnName = hit.transform.name;
+                switch (btnName)
+                {
+                    case "Food1":
+                        setPopupActive(1);
+                        break;
+                    case "Food2":
+                        setPopupActive(2);
+                        break;
+                    case "Food3":
+                        setPopupActive(3);
+                        break;
+                    case "Food4":
+                        setPopupActive(4);
+                        break;
+                    case "Menu1":
+                        selectMenu(1);
+                        break;
+                    case "Menu2":
+                        selectMenu(2);
+                        break;
+                    case "Menu3":
+                        selectMenu(3);
+                        break;
+                    default:
+                        setPopupActive(0);
+                        break;
+                }
+            }
+            else
+            {
+                setPopupActive(0);
+            }
+        }
+    }
+}
---
+++ Assets/MenuSelection.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSelection : MonoBehaviour {
+
+    public Color selected;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseOver()
+    {
+        GetComponentInChildren<Transform>().localScale = new Vector3(1.2f, 1.2f, 1);
+        //GetComponentInChildren<TextMesh>().color = new Color(1, 0, 0, 1);
+    }
+
+    private void OnMouseExit()
+    {
+        GetComponentInChildren<Transform>().localScale = new Vector3(1, 1, 1);
+        //GetComponentInChildren<TextMesh>().color = new Color(255, 165, 0, 255);
+    }
+
+    private void OnMouseDown()
+    {
+        //Debug.Log("Mouse clicked on a menu!");
+        //GetComponentInChildren<Transform>().localScale = new Vector3(1.2f, 1.2f, 1);
+        GetComponentInChildren<TextMesh>().color = selected;
+    }
+}
